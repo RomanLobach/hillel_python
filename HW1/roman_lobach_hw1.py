3ea9692fdc605f8a82e50ee2df24a8b64b8d4dff
Roman Lobach <r.lobach@inchkiev.ua>
2023-05-18 22:29:25 +0300

    hw1, hw2 added

---
+++ HW1/roman_lobach_hw1.py
@@ -0,0 +1,43 @@
+# 1 Задача: Створіть дві змінні first=10, second=30. Виведіть на екран результат математичної взаємодії (+, -, *, / и тд.) для цих чисел.
+
+first = 10
+second = 30
+
+print('addition: ', first + second)
+print('subtraction: ', first - second)
+print('multiplication: ', first * second)
+print('division: ', first / second)
+print('floor division: ', first // second)
+print('power: ', first ** second)
+print('remainder: ', first % second)
+
+# 2 Задача: Створіть змінну і почергово запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1. Виведіть на екран результат кожного порівняння.
+
+result = first < second
+print('< compare result: ', result)
+
+result = first > second
+print('> compare result: ', result)
+
+result = first <= second
+print('<= compare result: ', result)
+
+result = first >= second
+print('>= compare result: ', result)
+
+result = first == second
+print('== compare result: ', result)
+
+result = first != second
+print('!= compare result: ', result)
+
+# Задача: Створіть змінну - результат конкатенації (складання) строк str1="Hello " и str2="world". Виведіть на екран.
+
+str1 = "Hello "
+str2 = "world"
+
+conc_result = str1 + str2
+
+print(conc_result)
+
+# відповіді на додаткові питання (всі питання не памʼятаю): цікавив back-end, тому обирав між php, c#(asp.net) і python. Почитав вакансії, подивився зарплати, прикинув універсальність мов і обрав python. Тому взагалом хочу оволодіти актуальними технологіями back-end. У випадку basic курсу - вивчити основи і повторити ООП (в js зараз ООП не потрібно, тому починаю забувати). можливо зупинився би на Node. Але на нього ніде такого курсу немає, і в ютубі - тільки примітивні основи. Вірю, що такі курси дають також можливості отримати корисні знайомства. про жертву чомусь для мене смішне питання - схоже на питання "ким ви себе бачите через 5 років" на співбесіді. відповідь: гроші на курси, час на леккції, нерви на домашки.
