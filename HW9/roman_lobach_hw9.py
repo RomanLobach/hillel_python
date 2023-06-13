bdb980c551c81bb0c4955789424a01555eac83b7
Roman Lobach <r.lobach@inchkiev.ua>
2023-06-13 14:22:29 +0300

    HW9 added

---
+++ HW9/roman_lobach_hw9.py
@@ -0,0 +1,5 @@
+# Створіть файл lib.py, помістіть в нього функції вашої гри. Імпортуйте гру в основний файл і запустіть гру з основного файлу
+
+from lib import game as rock_scissors_paper_game
+
+rock_scissors_paper_game()
